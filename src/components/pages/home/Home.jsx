4184cff6e2a094c2ee9b58cd2dd6bf166e41b7c6
z3phyrondevs <z3phyrondevs@gmail.com>
2021-10-25 14:52:33 +0100

    ..

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,8 +3,11 @@ import styled from "styled-components";
 import showcase from "../../../assets/showcase.png";
 import applestore from "../../../assets/applestore.png";
 import googleplay from "../../../assets/googleplay.png";
-import "./index.css"
-import Slider from "../../helpers/marquee"
+import discover from "../../../assets/discover.svg";
+import invest from "../../../assets/invest.png";
+import track from "../../../assets/track.svg";
+import "./index.css";
+import Slider from "../../helpers/marquee";
 
 const Home = () => {
   return (
@@ -15,8 +18,8 @@ const Home = () => {
           <Desc>
             Bamboo gives unrestricted access to over 3,000 stocks listed on the
             U.S. stock exchange, right from your mobile phone or computer.
-                  </Desc>
-                  
+          </Desc>
+
           <BtnGrp>
             <button>
               <img classNmae="appstore" src={applestore} alt="" />
@@ -24,22 +27,61 @@ const Home = () => {
             <button>
               <img classNmae="google" src={googleplay} alt="" />
             </button>
-                  </BtnGrp>
-                  <Slider/>
+          </BtnGrp>
+          <Slider />
         </ShowcaseText>
         <ShowcaseImage>
           <img src={showcase} alt="" />
         </ShowcaseImage>
       </Showcase>
+      <DGrid>
+        <div className="image">
+          <img src={discover} alt="" />
+        </div>
+        <div className="text">
+          <Title className="title">Discover</Title>
+          <Desc>
+            Explore and choose from hundreds of U.S. stocks. We've made it fast
+            and seamless to discover the best local and global companies to
+            invest in.
+          </Desc>
+        </div>
+      </DGrid>
+      <DGrid className="opposite">
+        <div className="text">
+          <Title className="title">Invest</Title>
+          <Desc>
+            Fund your ₦ & $ balance almost instantly and start buying and
+            selling shares in just a few taps. You can also diversify your
+            holdings by buying into a bundle of stocks (called an ETF)
+          </Desc>
+        </div>
+        <div className="image">
+          <img src={invest} alt="" />
+        </div>
+      </DGrid>
+      <DGrid>
+        <div className="image">
+          <img src={track} alt="" />
+        </div>
+        <div className="text">
+          <Title className="title">Track</Title>
+          <Desc>
+            Intuitively designed for newcomers and experts alike, Bamboo gives
+            you a clear picture of your portfolio’s performance over time, so
+            you can adjust your positions and learn by doing.
+          </Desc>
+        </div>
+      </DGrid>
+      <Slider />
+     
     </Cont>
   );
 };
 
-const Cont = styled.div`
-
-`;
+const Cont = styled.div``;
 const Showcase = styled.div`
-  padding: 0  8%;
+  padding: 0 8%;
   display: grid;
   grid-template-columns: 41% auto;
   grid-gap: 50px;
@@ -74,7 +116,6 @@ const Desc = styled.p`
 const BtnGrp = styled.div`
   display: flex;
   align-items: center;
- 
 
   button {
     padding: 0;
@@ -94,8 +135,52 @@ const ShowcaseImage = styled.div`
   text-align: center;
   img {
     width: 100%;
-
   }
 `;
+const DGrid = styled.div`
+  display: flex;
+  justify-content: space-between;
+  grid-gap: 30px;
+  align-items: center;
+  padding: 8% 8%;
+
+  .text {
+    width: 40%;
+    @media (max-width: 900px) {
+      width: 100%;
+    }
+  }
+
+  .image {
+    width: 60%;
+    text-align: center;
+    @media (max-width: 900px) {
+      width: 100%;
+    }
+  }
+
+  img {
+    width: 90%;
+  }
+
+  &.opposite {
+    grid-template-columns: auto 60%;
+    img {
+      width: 100%;
+      transform: rotate(-17deg);
+    }
+  }
+
+  @media (max-width: 900px) {
+    flex-direction: column-reverse;
+    &.opposite {
+      flex-direction: column;
+    }
+  }
+`;
+
+// const SliderCont = styled.div`
+//   padding: 0 8%;
+// `;
 
 export default Home;
